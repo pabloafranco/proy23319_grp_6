cf38337f41be7cb86979b4aaa7d283300a3dc9c2
v nina <veronica.nina@softtek.com>
2023-04-18 19:22:45 -0300

    pruebas modulos

--- proyecto_23319_grp_6/publica/urls.py
+++ proyecto_23319_grp_6/publica/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hola_mundo/', views.hola_mundo, name='hola' ),
-    path('', views.index, name='index' ),
+    path('home/', views.home, name='home' ),
+    path('home/<int:idUser>/', views.homeLogIn, name='homeUser' ),
+    path('', views.home, name='home' ),
+    path('login/', views.login, name='login' ),
+    path('detailProduct/', views.detailProduct, name='detailProduct' )
+     
 ]
