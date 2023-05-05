ecd5eed361b82a7ca00b287bf71b1f7028c9bb29
pabloafranco <63153632+pabloafranco@users.noreply.github.com>
2023-05-05 15:11:25 -0300

    BorrarMain

--- proyecto_23319_grp_6/publica/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('hola_mundo/', views.hola_mundo, name='hola' ),
-    path('', views.index, name='index' ),
-]
--- proyecto_23319_grp_6/publica/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from . import views
-
-# Create your views here.
-def hola_mundo(request):
-    return HttpResponse('Hola Mundo django')
-def index(request):
-    return HttpResponse('Proyecto <h1>Django<h1>')
