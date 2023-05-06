f828dea50cb9133781e7d377147d718665da4d9e
RominaEgea <rosaromina90@hotmail.com>
2023-05-05 21:15:50 -0300

    actualizando

--- proyecto_23319_grp_6/manage.py
+++ proyecto_23319_grp_6/manage.py
@@ -20,3 +20,10 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carrito.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
